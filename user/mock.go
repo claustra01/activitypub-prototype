79ef6be6eaefecac9196ff0a68582b065c704026
claustra01 <shinya430.g@gmail.com>
2024-03-13 21:56:31 +0900

    add: generate pubpem

--- user/mock.go
+++ user/mock.go
@@ -1,6 +1,10 @@
 package user
 
 import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
 	"fmt"
 	"net/http"
 
@@ -12,6 +16,7 @@ type ActivityPubPerson struct {
 	Type              string    `json:"type"`
 	Id                string    `json:"id"`
 	Inbox             string    `json:"inbox"`
+	Outbox            string    `json:"outbox"`
 	PrefferedUsername string    `json:"prefferedUsername"`
 	Name              string    `json:"name"`
 	Summary           string    `json:"summary"`
@@ -25,14 +30,47 @@ type PublicKey struct {
 	PublicKeyPem string `json:"publicKeyPem"`
 }
 
+func generatePublicKeyPem() (string, error) {
+	// RSA鍵ペアを生成
+	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
+	if err != nil {
+		return "", fmt.Errorf("failed to generate RSA key pair: %w", err)
+	}
+
+	// 公開鍵をDER形式にエンコード
+	publicKeyBytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return "", fmt.Errorf("failed to encode public key: %w", err)
+	}
+
+	// DER形式の公開鍵をPEM形式に変換
+	publicKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: publicKeyBytes,
+	})
+
+	return string(publicKeyPEM), nil
+}
+
 func MockUser(c echo.Context) error {
 
 	url := fmt.Sprintf("https://%s/users/test", c.Get("host").(string))
+
+	// 公開鍵を生成
+	publicKeyPem, err := generatePublicKeyPem()
+	if err != nil {
+		return err
+	}
+
 	person := ActivityPubPerson{
-		Context:           []string{"https://www.w3.org/ns/activitystreams"},
+		Context: []string{
+			"https://www.w3.org/ns/activitystreams",
+			"https://w3id.org/security/v1",
+		},
 		Type:              "Person",
 		Id:                url,
 		Inbox:             url + "/inbox",
+		Outbox:            url + "/outbox",
 		PrefferedUsername: "test",
 		Name:              "Test User",
 		Summary:           "The user in activitypub server made by claustra01",
@@ -40,7 +78,7 @@ func MockUser(c echo.Context) error {
 			Type:         "Key",
 			Id:           url + "#main-key",
 			Owner:        url,
-			PublicKeyPem: "key is here",
+			PublicKeyPem: publicKeyPem,
 		},
 	}
 	c.Response().Header().Set("Content-Type", "application/activity+json")
